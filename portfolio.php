c96ebb3275de5c6953d2818a1938f97b1798b9b3
raybjork <raybjork@gmail.com>
2015-10-18 11:42:41 -0400

    Create portfolio.php

---
+++ portfolio.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Ray Bjorkman</title>
+		<link type="text/css" rel="stylesheet" href="css/main.css">
+	</head>
+	<body>
+		<nav>
+			<?php include "menu.php";?>
+		</nav>
+		<div class="container">
+    		<div class="heading">
+      			<h1>Projects</h1>
+    		</div>
+    		<p>But if I were a YUF would I have done these things?</p>
+		</div>
+	</body>
+</html>
