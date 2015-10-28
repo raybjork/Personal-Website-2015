07c1baa9d06d9c79da46cb1ce99fcf6cf088f1b6
raybjork <raybjork@gmail.com>
2015-10-28 16:39:42 -0400

    Update nav_bar.py

--- nav_bar.py
+++ nav_bar.py
@@ -7,14 +7,13 @@ keywords = ["<nav>", "</nav>"]
 
 text = '\n\
 			<ul>\n\
-	    		<li><a href="about.html">About</a></li>\n\
+	    		<li><a href="index.html">About</a></li>\n\
 	    		<li><a href="resume.html">Resume</a></li>\n\
 	    		<li><a href="portfolio.html">Projects</a></li>\n\
-	    		<li><a href="mailto:raybjork@gmail.com">Email</a></li>\n\
 			</ul>\n\
 		'
 
 #main script - cycle through directory and update files as needed
-for file in os.listdir(os.chdir(os.getcwd() + "\html")):
+for file in os.listdir(os.chdir(os.getcwd())):
 	if file.endswith(".html"):
 		html_preprocessor.preprocess(file, keywords, text)
